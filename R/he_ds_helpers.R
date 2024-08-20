f557e9c43c94783f30e00d2987e74c9d931c067c
JeffLansing <lansing2357@gmail.com>
2024-08-20 15:37:36 -0700

    Added rgl display function.

--- R/he_ds_helpers.R
+++ R/he_ds_helpers.R
@@ -78,3 +78,47 @@ get_display_info <- function(info) {
     matrix(byrow = TRUE, ncol = 1)
   list(vsc = vsc, vsf = vsf, vs = vs, ts = ts)
 }
+
+#' display_hes
+#' Display half-edge data structures with rgl.
+#'
+#' @param hes A list of half-edge data structures
+#' @param rc The number of rows and columns in the displayl r * c = length(hes)
+#' @param shared Whether the rgl mouse interactions are shared or not
+#' @param special a boolean indicating whether to show simplified view,
+#' without cut and fold information.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+display_hes <- function(hes, rc, shared = FALSE, special = FALSE) {
+  open3d(windowRect = c(50, 50, 750, 350 * rc[1]))
+  view3d(20, 10, zoom = 0.85)
+  par3d(font = 2, FOV = 10) #0 is isomorphic view
+  material3d(color = 'white', alpha = 0.1)
+  mfrow3d(rc[1], rc[2], sharedMouse = shared)
+  for(i in 1:length(hes)) {
+    he <- hes[[i]]
+    einfo <- he %>% edgelist_from_he_ds() %>% get_display_info()
+    cinfo <- he %>% coedgelist_from_he_ds() %>% get_display_info()
+    next3d()   # won't advance the first time, since it is empty
+    bgplot3d({
+      plot.new()
+    })
+    if(special) {
+      segments3d(einfo$vs, col="black", lwd=1)
+      segments3d(cinfo$vs, col="red", lwd=1)
+    } else {
+      segments3d(einfo$vsc, col="black", lwd=1)
+      segments3d(einfo$vsf, col="yellow", lwd=1)
+      segments3d(cinfo$vsf, col="red", lwd=1)
+      segments3d(cinfo$vsc, col="lightblue", lwd=1)
+    }
+    text3d(einfo$vs, texts = str_c('v', einfo$ts), adj = 1.1)
+    text3d(cinfo$vs, texts = str_c('f', cinfo$ts), col="red", adj = 1.1)
+  }
+  highlevel(integer()) # To trigger display as rglwidget
+}
+
